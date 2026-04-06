2a025e61c11b6ae36816ed796e5da8adc083cd2f
Yuqing Zhai <kevinzhai2003@outlook.com>
2026-04-06 14:21:35 -0500

    feat(fast-cli): add commands barrel for array dispatch

---
+++ app/cli/src/commands/index.ts
@@ -0,0 +1,35 @@
+import { accountCreate } from "./account/create.js";
+import { accountDelete } from "./account/delete.js";
+import { accountExport } from "./account/export.js";
+import { accountImport } from "./account/import.js";
+import { accountInfo } from "./account/info.js";
+import { accountList } from "./account/list.js";
+import { accountSetDefault } from "./account/set-default.js";
+import { infoBalance } from "./info/balance.js";
+import { infoHistory } from "./info/history.js";
+import { infoStatus } from "./info/status.js";
+import { infoTx } from "./info/tx.js";
+import { networkAdd } from "./network/add.js";
+import { networkList } from "./network/list.js";
+import { networkRemove } from "./network/remove.js";
+import { networkSetDefault } from "./network/set-default.js";
+import { send } from "./send.js";
+
+export const commands = [
+  accountCreate,
+  accountDelete,
+  accountExport,
+  accountImport,
+  accountInfo,
+  accountList,
+  accountSetDefault,
+  infoBalance,
+  infoHistory,
+  infoStatus,
+  infoTx,
+  networkAdd,
+  networkList,
+  networkRemove,
+  networkSetDefault,
+  send,
+] as const;
